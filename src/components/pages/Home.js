a7612ea9d36e213d3e8278d1f762514e1d19ceae
Latifah Zawedde <zawedde2021@gmail.com>
2022-10-26 09:43:49 -0700

    creatinghom ad about

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'raect';
+
+export default function Home() {
+    return (
+        <div>
+            <p>
+                sderfgthyujikolkjhgfcdxghjklm., bnvcfghjklm, nbvcgfhjk,mnb vgfhjk
+                dfghjkl;'/,m,nbvcgfhjhkjl.,
+                xdxfghjkl;.'/,mkbvgcbfxdfyuh
+                dfghjkl;/lkjhbgfchjkl.
+                dfghjkl;/,.mnbvcdfsthyhujkl;
+                dfghjkl;/.lkjhbgfchjkldsetruiok;l,mnvfdsagwte4i67.'
+            </p>
+        </div>
+    );
+}
