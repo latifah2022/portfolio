c56be9ccb3076043c64599d5e219a05878be901e
Latifah Zawedde <zawedde2021@gmail.com>
2022-10-29 14:09:14 -0700

    navbar components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,2 +1,29 @@
 import React from 'react';
-import {}
+import { navigation } from '../data';
+import { Link } from 'react-scroll';
+
+const Navbar = () => {
+    return <nav>
+        <ul className="flex space-x-8 capitalize text-[15px]">
+            {navigation.map((item, index) => {
+                return (
+                    <li className="text-white hover:text-accent cursor-pointer"key={idx}>
+                    <Link
+                        to={item.href}
+                        activeClass='active'
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        offset={-70}
+                        className='transition-all duration-300'
+                    >
+                    {item.name}
+                    </Link>
+                    </li>
+                );
+            })}
+        </ul>
+    </nav>;
+}
+
+export default Navbar;
