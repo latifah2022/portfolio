f527b00d93331ab47f17e4ef62e3fe471c99d6ba
Latifah Zawedde <zawedde2021@gmail.com>
2022-10-29 16:17:55 -0700

    footer components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,18 +22,19 @@ const Contact = () => {
                     {contact.map((item, index) => {
                         const { icon, title, subtitle, description } = item;
                             return (
-                                    <div className='flex flex-col lg:flex-row gap-x-4' key={index}>
-                                        <div className='text-accent rounded-sm w-14 h-14 flex items-start justify-center mt-2 mb-4 lg:mb-0 text-2xl'>
-                                            {icon}
-                                        </div>
-                                        <div>
-                                            <h4 className='font-body text-xl mb-1'>{title}</h4>
-                                            <p className='mb-1 text-paragraph'>{subtitle}</p>
-                                            <p className='text-accent font-normal '>{description}</p>
-                                        </div>
+                                <div className='flex flex-col lg:flex-row gap-x-4' key={index}>
+                                    <div className='text-accent rounded-sm w-14 h-14 flex items-start justify-center mt-2 mb-4 lg:mb-0 text-2xl'>
+                                        {icon}
                                     </div>
-                                     );
-                                })}
+                                    <div>
+                                        <h4 className='font-body text-xl mb-1'>{title}</h4>
+                                        <p className='mb-1 text-paragraph'>{subtitle}</p>
+                                        <p className='text-accent font-normal '>{description}</p>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
                 </div>
                 <form className='space-y-8 w-full max-w-[780px]'>
                     <div className='flex gap-8'>
