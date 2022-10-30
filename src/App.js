5820e60efef1c5bf2951a5fd9c418f6af603103f
Latifah Zawedde <zawedde2021@gmail.com>
2022-10-30 10:14:09 -0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
+import React from 'react';
+
+// import components
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Port from './components/Port';
+import Skills from './components/Skills';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Scroll from './components/Scroll';
+
+const App = () => {
+  return (
+    <div className='bg-white relative'>
+      <Header />
+      <Home />
+      <About />
+      <Skills />
+      <Port />
+      <Services />
+      <Contact />
+      <Footer />
+      <Scroll />
+    </div>
+  );
+};
+
+export default App;
