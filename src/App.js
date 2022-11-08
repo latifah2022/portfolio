a2678c349d97815d8c7ea5a25d87673fe2e8c3f7
Latifah Zawedde <zawedde2021@gmail.com>
2022-11-08 12:11:54 -0800

    components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React from 'react';
 
 // import components
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import Port from './components/Port';
-import Skills from './components/Skills';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Scroll from './components/Scroll';
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Port from './components/Port';
+// import Skills from './components/Skills';
+// import Services from './components/Services';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import Scroll from './components/Scroll';
 
 const App = () => {
   return (
-    <div className='bg-white relative'>
-      <Header />
-      <Home />
-      <About />
-      <Skills />
-      <Port />
-      <Services />
-      <Contact />
-      <Footer />
-      <Scroll />
-      <div style={{ height: "2000px"}}></div>
-
+    <div className='bg-blue relative '>
+      
     </div>
   );
 };
 
 export default App;
+
+// {/* <Header />
+//       <Home />
+//       <About />
+//       <Skills />
+//       <Port />
+//       <Services />
+//       <Contact />
+//       <Footer />
+//       <Scroll />
+//       <div style={{ height: "2000px"}}></div> */}
