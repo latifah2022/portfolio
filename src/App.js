33effd9a51d54e999a3c94f45a9136fbfb1eb025
Latifah Zawedde <zawedde2021@gmail.com>
2022-11-10 16:39:32 -0800

    created home js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-import Header from "./componets/Header"
+import Header from "./componets/Header";
+import Home from "./componets/Home";
+
 function App() {
   return (
     <div className="text-4xl font-bold ">
       <Header />
+      <Home />
     </div>
   );
 }
